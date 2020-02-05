f011c0f4efefcc5c5012b3dd1ce62b9bdddf82ab
Gwendall Esnault <gwendall.esnault@gmail.com>
2020-02-05 14:38:20 +0100

    version bump

--- package.js
+++ package.js
@@ -1,19 +1,11 @@
 Package.describe({
   name: "gwendall:body-events",
   summary: "Get Template.body.events() working",
-  version: "0.1.6",
+  version: "0.1.7",
   git: "https://github.com/gwendall/meteor-body-events"
 });
 
-Package.on_use(function (api, where) {
-
-  api.use([
-    "templating@1.0.7",
-    "jquery@1.0.0"
-  ], "client");
-
-  api.add_files([
-    "lib.js"
-  ], "client");
-
+Package.on_use(function(api, where) {
+  api.use(["templating@1.0.7", "jquery@1.0.0"], "client");
+  api.add_files(["lib.js"], "client");
 });
